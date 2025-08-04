482dcd98e99e4f801c2f88a41194be459d6e1b63
skkrishnamohan <skkrishnamohan@gmail.com>
2025-08-04 18:18:42 +0530

    Refactor heloworld.java to include Person class and enhance main method functionality

--- heloworld.java
+++ heloworld.java
@@ -1,5 +1,27 @@
-public class heloworld {
-    publistactic void main(String[] args) {
+class Person {
+    public String cname;
+    private int cage;
+
+    void getName(String name, int age) {
+        this.cname = name;
+        this.cage = age;
+    }
+
+    void sayHello() {
+        System.out.println("Hello my name is " + cname + " and I am " + cage + " years old. I am a Java developer.");
+    }
+}
+
+class heloworld {
+    public static void main(String[] args) {
         System.out.println("Hello, World!");
+        Person obj=new Person();
+        Person obj1=new Person();
+        
+    
+        obj.getName("Suba", 23);
+        obj1.getName("Dhanu", 25);
+        obj.sayHello();
+    
     }
 }
